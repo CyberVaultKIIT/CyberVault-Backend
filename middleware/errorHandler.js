760828274dd742c803d893c7d8e26741c5700961
DebSaha141 <debsaha141@gmail.com>
2024-12-07 00:00:02 +0530

    Added a Middleware function to Handle Errors

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,4 +1,29 @@
-module.exports = (err, req, res, next) => {
+function errorHandler(err, req, res, next) {
+
+  var statusCode;
+  var message;
+
+  if (err.statusCode) {
+    statusCode = err.statusCode;
+  } else {
+    statusCode = 500;
+  }
+
+  if (err.message) {
+    message = err.message;
+  } else {
+    message = 'Internal Server Error';
+  }
+
   console.error(err.stack);
-  res.status(500).send('Something broke!');
-};
+
+  res.status(statusCode).json({
+      success: false,
+      error: {
+          message: message,
+          stack: err.stack,
+      },
+  });
+}
+
+module.exports = errorHandler;
