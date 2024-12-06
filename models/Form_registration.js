3d3692f6c1897b43e558628fa305782e0d36f444
Debdyuti Chakraborty <168359578+Debdyuti2004@users.noreply.github.com>
2024-12-06 22:17:12 +0530

    camel style implemented

--- models/Form_registration.js
+++ models/Form_registration.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
 
 const FormRegistrationSchema = new mongoose.Schema({
-  ID: { type: String, required: true, unique: true }, // Primary Key
-  UserId: { type: String, required: true },
-  Team_id: { type: String, required: true },
-  CreatedAt: { type: Date, default: Date.now },
-  UpdatedAt: { type: Date, default: Date.now },
-  Response: { type: mongoose.Schema.Types.Mixed }, // Mixed type for flexibility
+  id: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  }, 
+  userId: { 
+    type: String, 
+    required: true 
+  },
+  team_id: { 
+    type: String, 
+    required: true 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+  updatedAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+  response: { 
+    type: mongoose.Schema.Types.Mixed 
+  }, 
 });
 
 module.exports = mongoose.model('FormRegistration', FormRegistrationSchema);
