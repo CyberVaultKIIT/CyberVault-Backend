b0515927dc8d53517840daa82095c04aa8a32b41
DebSaha141 <debsaha141@gmail.com>
2025-01-29 21:26:08 +0530

    Addded Form GetRoute, SubmitResponseRoute and Cloudinary Image Upload

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-//const registrationController = require('../controllers/registrationController');
-const {createForm}=require('../controllers/Form/createFormController.js')
-const {deleteForm}=require('../controllers/Form/deleteFormController.js')
-const {updateForm}=require('../controllers/Form/updateFormController.js')
+const registrationController = require('../controllers/registrationController');
+const { createForm } = require("../controllers/Form/createFormController.js");
+const { deleteForm } = require("../controllers/Form/deleteFormController.js");
+const { updateForm } = require("../controllers/Form/updateFormController.js");
+const multer = require("multer");
+const { storage } = require('../cloudinary/config.js');
+const upload = multer({ storage });
+const catchAsync = require("../utils/catchAsyncError.js");
 
-//router.post('/saveResponse', registrationController.saveResponse);
-router.post('/createForm', createForm) 
-router.delete('/deleteForm', deleteForm)
-router.put('/updateForm', updateForm)
+// router.post('/saveResponse', registrationController.saveResponse);
+router.route("/:formId").get(catchAsync(registrationController.getForm)).post(upload.array("image"),catchAsync(registrationController.saveResponse));
+
+
+router.post("/createForm", createForm);
+router.delete("/deleteForm/:formId", deleteForm);
+router.put("/updateForm/:formId", updateForm);
 // Additional routes can be added here
 
 module.exports = router;
