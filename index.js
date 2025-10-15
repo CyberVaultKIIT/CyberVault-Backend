1199294253b7d9a7ec211cd50089557ea4cd115c
manika <manikamanipathak@gmail.com>
2025-10-16 00:43:28 +0530

    fixed the routing for teams

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const Logger = require('./utils/Logger')
 const app = express()
 const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
-const cors = require('cors')
 
 require('dotenv').config()
 app.use(express.json())
 app.use(cookieParser())
 connectDB()
 
-app.use(cors());
+// Set CORS before all routes, only allow frontend origin with credentials
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 5000
 
 app.use('/api', require('./routes/api'))
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'server is running' })
